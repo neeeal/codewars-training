1093647d44e590975d99409c1df2d8c52b6de129
Niil <91337648+neeeal@users.noreply.github.com>
2024-01-25 16:08:37 +0800

    includes only alphanumeric characters

--- under_name.py
+++ under_name.py
@@ -1,7 +1,7 @@
 import sys
 
 def string_underscore(string):
-    return '_'.join(string.strip().split(' ')).lower()
+    return '_'.join(''.join([char for char in word if char.isalnum()]) for word in string.strip().split(' ')).lower()
 
 if __name__ == '__main__':
     print(string_underscore(sys.argv[1]))
