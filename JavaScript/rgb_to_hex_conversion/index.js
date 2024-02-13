746e8ab7a43f228c6c6cee61699aec0e49a9da57
Niil <91337648+neeeal@users.noreply.github.com>
2024-02-13 23:03:57 +0800

    finished rgb_to_hex_conversion

--- JavaScript/rgb_to_hex_conversion/index.js
+++ JavaScript/rgb_to_hex_conversion/index.js
@@ -1,4 +1,11 @@
 function rgb(r, g, b) {
-  return "FFFFFF";
+    const HEX = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+    return [r,g,b].reduce((acc,curr)=>{
+        return curr < 0 ? acc + '00'
+        : curr > 255 ? acc + 'FF'
+        :acc + `${HEX[Math.floor(curr/16)%16]}` + `${HEX[curr%16]}`      
+        }
+    , ''
+    )
 }
   module.exports = rgb
